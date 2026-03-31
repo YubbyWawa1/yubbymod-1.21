edae4cd4b83cafc4d37b5d8937d6ec8d97e5759c
YubbyWawa <wawa13529@gmail.com>
2026-03-31 16:17:52 -0500

    Initial commit

---
+++ src/main/java/net/yubbywawa/yubbymod/YubbyMod.java
@@ -0,0 +1,16 @@
+package net.yubbywawa.yubbymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class YubbyMod implements ModInitializer {
+	public static final String MOD_ID = "yubbymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/yubbywawa/yubbymod/YubbyModClient.java
@@ -0,0 +1,11 @@
+package net.yubbywawa.yubbymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YubbyModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
